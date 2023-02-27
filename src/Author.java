2f0db2868dfda5fdfaea0632f73ae9772a5bf56c
matvey <qjlf@inbox.ru>
2023-02-27 20:42:00 +0300

    Прошу проверить ДЗ

---
+++ src/Author.java
@@ -0,0 +1,17 @@
+public class Author {
+    private String author;
+
+
+    public Author(String author) {
+
+        this.author = author;
+
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor() {
+        this.author = author;
+    }
+}
